370d9b3ecbe02b49d131332335da754ad24a8f0f
Sagar Karmoker <sagarkarmoker2001@gmail.com>
2024-08-30 19:51:13 +0600

    lending page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Navbar from "./components/ui/Navbar";
+import LendingPage from "./components/ui/LendingPage";
 
 export default function Home() {
   return (
     <main className="">
-      <Navbar />
+      {/* <Navbar /> */}
+      <LendingPage />
     </main>
   );
 }
